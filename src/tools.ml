f3393db573763e5cabd0ee0bbe1e4425a3f3ed03
ThibaultES <thibault.es@free.fr>
2025-11-19 11:12:40 +0100

    Merge done

---
+++ src/tools.ml
@@ -0,0 +1,9 @@
+open Graph
+
+let clone_nodes gr = Graph.n_fold gr (fun g x -> Graph.new_node g x) Graph.empty_graph 
+let gmap gr f = 
+  e_fold gr  (fun g2 arc ->  (Graph.new_arc g2 {src = arc.src; tgt = arc.tgt; lbl = (f arc.lbl)}))  (clone_nodes gr)
+
+let add_arc g id1 id2 n = match find_arc g id1 id2 with 
+| None -> new_arc g {src = id1; tgt = id2; lbl = n}
+| Some(arc) -> new_arc g {src = id1; tgt = id2; lbl = arc.lbl + n}
