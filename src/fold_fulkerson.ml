5bc7fb7b1c5e60f2c055c2f28803f31e50ec94cc
corentin <corentin@narvalo.rev.sfr.net>
2025-12-04 11:45:18 +0100

    bfs

--- src/fold_fulkerson.ml
+++ src/fold_fulkerson.ml
@@ -15,3 +15,16 @@ let gap_graph graph = (* gr : graphe dont les labels sont des tuples (flux, capa
 
 let test_graph graph =
   gmap graph (fun id -> (id / 2, id))
+
+let bfs graph s p =
+  let q = Queue.add (Queue.create) s in 
+  let paths = Array.make (Graph.size graph) [] in 
+  let rec aux_bfs () = match Queue.take_opt queue with
+  | None -> failwith "No path was found" (* Il n'y a pas de chemin *)
+  | Some v when p = v -> paths.(p)  (* On renvoie le chemin *)
+  | Some v -> let arcs_out = out_arcs graph v in
+      List.iter (fun arc -> Queue.add queue arc; (* Pour chaque voisin non visité : l'ajouter à la file *)
+                            v::paths.(arc.tgt)) arcs_out ; (* On ajoute v comme parent au chemin de ses voisins *) 
+      aux_bfs() (* Itération suivante *)
+  in aux_bfs graph q []
+   
