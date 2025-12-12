7d244d3699f2efdbf8cd2b07fb039e587a997551
ThibaultES <thibault.es@free.fr>
2025-12-12 12:06:56 +0100

    ajout algo ford_fulkerson

--- src/fold_fulkerson.ml
+++ src/fold_fulkerson.ml
@@ -17,6 +17,16 @@ let test_graph graph =
   gmap graph (fun id -> (id / 2, id))
 
 
+let flow_graph capa_graph gap_graph =
+  e_fold gap_graph (fun g arc -> match find_arc capa_graph arc.src arc.tgt with
+    | None -> g
+    | Some arc_capa -> new_arc g {src = arc.src; tgt = arc.tgt; lbl = "("^(string_of_int arc.lbl)^"/"^(string_of_int arc_capa.lbl)^")"}
+  ) (clone_nodes gap_graph)
+
+
+
+
+
 let get_min_capa graph path = 
   let rec aux_get_min_capa graph path mini = match path with 
   |[] -> failwith "path is empty"
@@ -51,4 +61,11 @@ let rec update_graph graph path flow_to_add = match path with
   | x::y::p -> let graphXY = (add_arc graph x y flow_to_add) in let graphYX  = (add_arc graphXY y x (-flow_to_add)) in
     update_graph graphYX (y::p) flow_to_add
     
+
+let ford_fulkerson graph s p =
+  let rec aux_fulkerson gap_graph = 
+  let path = bfs gap_graph s p in match path with
+    | [] -> gap_graph
+    | _ -> aux_fulkerson (update_graph gap_graph path (get_min_capa gap_graph path))
+in flow_graph graph (aux_fulkerson graph)
 
